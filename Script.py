344f33f47dc6dbca47f0df861a95828477e14620
devilphantom <phantomdevilbot73@gmail.com>
2025-12-07 20:25:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -225,7 +225,7 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 <b>{file_name}</b></a
 
 <b><blockquote>𝙿𝚘𝚠𝚎𝚛e𝚍 𝙱𝚢 ➥ <a href="https://t.me/MovieHuntZone">
-𝐔𝐏𝐃𝐀𝐓𝐄 𝐂𝐇𝐇𝐀𝐍𝐄𝐋
+𝐔𝐏𝐃𝐀𝐓𝐄 CHANNEL
 </a></blockquote></b><b><blockquote>𝙿𝚘𝚠𝚎𝚛e𝚍 𝙱𝚢 ➥ <a href="https://t.me/+SzLcULYfU8s4OTM1">
 @𝐌𝐎𝐕𝐈𝐄 𝐑𝐄𝐐𝐔𝐄𝐒𝐓 𝐆𝐑𝐎𝐔𝐏</a></blockquote></b>"""
 
